1d3ac2053a879736f823ec063503e9e21f1d506c
Maarten Everts <me@nn8.nl>
2016-04-17 15:24:10 +0200

    Cleanup of keys.go
    Support for encoding/decoding keys in XML format. General cleanup and
    addition of comments.

--- keys.go
+++ keys.go
@@ -1,14 +1,25 @@
 package credential
 
 import (
+	"encoding/xml"
 	"math/big"
 )
 
+const (
+	//XMLHeader can be a used as the XML header when writing keys in XML format.
+	XMLHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
+)
+
+// SecretKey represents an issuer's secret (private) key.
 type SecretKey struct {
-	P, Q           big.Int
-	PPrime, QPrime big.Int
+	XMLName xml.Name `xml:"http://www.zurich.ibm.com/security/idemix IssuerPrivateKey"`
+	P       big.Int  `xml:"Elements>p"`
+	Q       big.Int  `xml:"Elements>q"`
+	PPrime  big.Int  `xml:"Elements>pPrime"`
+	QPrime  big.Int  `xml:"Elements>qPrime"`
 }
 
+// NewSecretKey creates a new issuer secret key using the provided parameters.
 func NewSecretKey(p, q *big.Int) *SecretKey {
 	sk := SecretKey{P: *p, Q: *q}
 
@@ -21,19 +32,89 @@ func NewSecretKey(p, q *big.Int) *SecretKey {
 	return &sk
 }
 
-type PublicKey struct {
-	N      big.Int // Modulus n
-	Z      big.Int // Generator Z
-	S      big.Int // Generator S
-	R      []*big.Int
-	Params *SystemParameters
+// xmlBases is an auxiliary struct to encode/decode the odd way bases are
+// represented in the xml representation of public keys
+type xmlBases struct {
+	Num   int      `xml:"num,attr"`
+	Base0 *big.Int `xml:"Base_0"`
+	Base1 *big.Int `xml:"Base_1"`
+	Base2 *big.Int `xml:"Base_2"`
+	Base3 *big.Int `xml:"Base_3"`
+	Base4 *big.Int `xml:"Base_4"`
+	Base5 *big.Int `xml:"Base_5"`
 }
 
+// xmlFeatures is an auxiliary struct to make the XML encoding/decoding a bit
+// easier while keeping the struct for PublicKey somewhat simple.
+type xmlFeatures struct {
+	Epoch struct {
+		Length int `xml:"length,attr"`
+	}
+}
+
+// Bases is a type that is introduced to simplify the encoding/decoding of
+// a PublicKey whilst using the xml support of Go's standard library.
+type Bases []*big.Int
+
+// UnmarshalXML is an internal function to simplify decoding a PublicKey from
+// XML.
+func (bl *Bases) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var t xmlBases
+
+	if err := d.DecodeElement(&t, &start); err != nil {
+		return err
+	}
+	*bl = Bases{t.Base0, t.Base1, t.Base2, t.Base3, t.Base4, t.Base5}
+	return nil
+}
+
+// MarshalXML is an internal function to simplify encoding a PublicKey to XML.
+func (bl *Bases) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	t := xmlBases{Num: len(*bl), Base0: (*bl)[0], Base1: (*bl)[1], Base2: (*bl)[2], Base3: (*bl)[3], Base4: (*bl)[4], Base5: (*bl)[5]}
+	return e.EncodeElement(t, start)
+}
+
+// EpochLength is a type that is introduced to simplify the encoding/decoding of
+// a PublicKey whilst using the xml support of Go's standard library.
+type EpochLength int
+
+// UnmarshalXML is an internal function to simplify decoding a PublicKey from
+// XML.
+func (el *EpochLength) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var t xmlFeatures
+
+	if err := d.DecodeElement(&t, &start); err != nil {
+		return err
+	}
+	*el = EpochLength(t.Epoch.Length)
+	return nil
+}
+
+// MarshalXML is an internal function to simplify encoding a PublicKey to XML.
+func (el *EpochLength) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	var t xmlFeatures
+	t.Epoch.Length = int(*el)
+	return e.EncodeElement(t, start)
+}
+
+// PublicKey represents an issuer's public key.
+type PublicKey struct {
+	XMLName     xml.Name    `xml:"http://www.zurich.ibm.com/security/idemix IssuerPublicKey"`
+	N           big.Int     `xml:"Elements>n"` // Modulus n
+	Z           big.Int     `xml:"Elements>Z"` // Generator Z
+	S           big.Int     `xml:"Elements>S"` // Generator S
+	R           Bases       `xml:"Elements>Bases"`
+	EpochLength EpochLength `xml:"Features"`
+	Params      *SystemParameters
+}
+
+// NewPublicKey creates and returns a new public key based on the provided parameters.
 func NewPublicKey(N, Z, S *big.Int, R []*big.Int) *PublicKey {
 	pk := PublicKey{N: *N, Z: *Z, S: *S, R: R, Params: &DefaultSystemParameters}
 	return &pk
 }
 
+// BaseParameters holds the base system parameters
 type BaseParameters struct {
 	Le      uint
 	LePrime uint
@@ -54,6 +135,8 @@ var defaultBaseParameters = BaseParameters{
 	Lv:      1700,
 }
 
+// DerivedParameters holds system parameters that can be drived from base
+// systemparameters (BaseParameters)
 type DerivedParameters struct {
 	LeCommit      uint
 	LmCommit      uint
@@ -64,6 +147,7 @@ type DerivedParameters struct {
 	LvPrimeCommit uint
 }
 
+// makeDerivedParameters computes the derived system parameters
 func makeDerivedParameters(base BaseParameters) DerivedParameters {
 	return DerivedParameters{
 		LeCommit:      base.LePrime + base.Lstatzk + base.Lh,
@@ -76,13 +160,17 @@ func makeDerivedParameters(base BaseParameters) DerivedParameters {
 	}
 }
 
+// SystemParameters holds the system parameters of the IRMA system.
 type SystemParameters struct {
 	BaseParameters
 	DerivedParameters
 }
 
+// DefaultSystemParameters holds the default parameters as are currently in use
+// at the moment. This might (and probably will) change in the future.
 var DefaultSystemParameters = SystemParameters{defaultBaseParameters, makeDerivedParameters(defaultBaseParameters)}
 
+// ParamSize computes the size of a parameter in bytes given the size in bits.
 func ParamSize(a int) int {
 	return (a + 8 - 1) / 8
 }
