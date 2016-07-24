234a7f1297f14bb7336c7d4d92192e5cc18dba53
Maarten Everts <me@nn8.nl>
2016-07-24 19:41:59 +0200

    Add LICENSE and references to it in source files.

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Maarten Everts. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package gabi is an implementation of the IRMA (https://irmacard.org) approach
 // to attribute based credentials.
 package gabi
