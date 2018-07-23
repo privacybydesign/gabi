b758aaeb2e3db35b35bee6d52f358abb7257a72c
Sietse Ringers <mail@sietseringers.net>
2018-07-24 00:34:16 +0200

    Improve safeprime unit test

--- safeprime/safeprime_test.go
+++ safeprime/safeprime_test.go
@@ -3,24 +3,19 @@ package safeprime
 import (
 	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenerate(t *testing.T) {
 	x, err := Generate(1024)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Log(x.String())
-
-	if !x.ProbablyPrime(100) {
-		t.Fatal("Generated number was not prime")
-	}
+	require.NoError(t, err)
+	require.NotNil(t, x)
+	require.True(t, x.ProbablyPrime(100), "Generated number was not prime")
 
 	y := new(big.Int).Sub(x, big.NewInt(1))
 	y.Div(y, big.NewInt(2))
-	if !y.ProbablyPrime(100) {
-		t.Fatal("(prime-1)/2 was not prime")
-	}
+
+	require.True(t, y.ProbablyPrime(100), "Generated number was not a safe prime")
 }
