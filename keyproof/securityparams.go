56b980ca8d63683408efe4b05a1a6e7bbd85c8e8
David Venhoek <d.venhoek@privacybydesign.foundation>
2019-12-13 13:31:21 +0100

    Clarified error probability comments on security parameters.

--- keyproof/securityparams.go
+++ keyproof/securityparams.go
@@ -1,12 +1,12 @@
 package keyproof
 
 const almostSafePrimeProductNonceSize = 256 // Probably not that important
-const almostSafePrimeProductIters = 250     // needed since error prob is 4/5
-const disjointPrimeProductIters = 8         // error prob of 1/minimumFactor
-const primePowerProductIters = 80           // error prob of 1/2
-const squareFreeIters = 8                   // error prob of 1/minimumFactor
+const almostSafePrimeProductIters = 250     // needed since error prob is 4/5 per iter
+const disjointPrimeProductIters = 8         // error prob of 1/minimumFactor per iter
+const primePowerProductIters = 80           // error prob of 1/2 per iter
+const squareFreeIters = 8                   // error prob of 1/minimumFactor per iter
 
 const minimumFactor = 1024
 
-const rangeProofIters = 80    // Binary challenge, so error rate of 1/2
+const rangeProofIters = 80    // Binary challenge, so error rate of 1/2 per iter
 const rangeProofEpsilon = 256 // Number of bits for statistical hiding
